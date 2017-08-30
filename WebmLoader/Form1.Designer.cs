01c256b45f4cfa7e8a799822e83856dfcc258573
m0rn <morn88@gmail.com>
2017-08-30 22:50:19 +0400

    Добавьте файлы проекта.

---
+++ WebmLoader/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WebmLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // linkBox
+            // 
+            this.linkBox.Location = new System.Drawing.Point(12, 12);
+            this.linkBox.Name = "linkBox";
+            this.linkBox.Size = new System.Drawing.Size(321, 20);
+            this.linkBox.TabIndex = 0;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(339, 9);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 1;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 64);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(94, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Вставьте ссылку";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 121);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(399, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCount.Location = new System.Drawing.Point(339, 63);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(0, 16);
+            this.labelCount.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 162);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.linkBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox linkBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelCount;
+    }
+}
+
---
+++ WebmLoader/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace WebmLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (linkBox.TextLength != 0)
+            {
+                statusLabel.Text = "Loading";
+                int count = 0;
+                string remoteUrl = linkBox.Text;
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadFile(remoteUrl, "links.txt");
+                statusLabel.Text = "Done!";
+                string text = System.IO.File.ReadAllText(path: "links.txt");
+                string pattern = @"\/\w+\/\w+\/\w+\/\d+\.(webm|mp4)";
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(text);
+                HashSet<string> urls = new HashSet<string>();
+                while (match.Success)
+                {
+                    urls.Add("https://2ch.hk" + match.Value);
+                    match = match.NextMatch();
+                    count++;
+                }
+                progressBar1.Maximum = urls.Count;
+                labelCount.Text = urls.Count.ToString();
+                foreach (string a in urls)
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                        wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+                        wc.DownloadFileAsync(new Uri(a), @"done\" + a.Split('/').Last());
+                    }
+                }
+            }
+        }
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // In case you don't have a progressBar Log the value instead 
+            // Console.WriteLine(e.ProgressPercentage);
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+
+            if (e.Cancelled)
+            {
+                MessageBox.Show("The download has been cancelled");
+                return;
+            }
+
+            if (e.Error != null) // We have an error! Retry a few times, then abort.
+            {
+                MessageBox.Show("An error ocurred while trying to download file");
+
+                return;
+            }
+        }
+    }
+}
